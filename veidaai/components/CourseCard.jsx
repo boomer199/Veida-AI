6b716d6525d670892f17362f2bf260b7c65aa88f
Derek Gomez <demagome@ucsc.edu>
2025-02-14 20:07:27 -0800

    transferring styles to .css file for the CourseCard component

--- veidaai/components/CourseCard.jsx
+++ veidaai/components/CourseCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import EditButton from './EditButton';
+import './CourseCard.css';
 
 export default function CourseCard({name, descr}) {
   return (
     <div className="course-card">
-        <EditButton />
-        <h3 className="course-name">{name}</h3>
-        <p>{descr}</p>
+      <EditButton />
+      <h3 className="course-name">{name}</h3>
+      <p>{descr}</p>
     </div>
   )
 }
