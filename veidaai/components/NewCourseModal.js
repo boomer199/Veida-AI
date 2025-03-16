6fb0223f347733dab62195930c112fa2819be82f
Derek Gomez <demagome@ucsc.edu>
2025-03-16 04:25:25 -0700

    updated for implementation - handleClick props added and assigned - placeholder text fixed

--- veidaai/components/NewCourseModal.js
+++ veidaai/components/NewCourseModal.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import './NewCourseModal.css';
+import './Modal.css';
 
-export default function NewCourseModal() {
+export default function NewCourseModal({onCourseCreated, onClose}) {
   return (
     <div className="modal-container">
-      <button className="x"><img src="./icons/x.svg" alt="Close"></img></button>
+      
+      <button className="x" onClick={onClose}>
+        <img src="./icons/x.svg" alt="Close"></img>
+      </button>
       
       <h2 className="modal-title">Create New Course</h2>
       <p className="modal-subtitle">Provide the course name and a brief description to get started. You can update these details later if needed</p>
       
-      <form className="modal-form">
+      <form className="modal-form" onSubmit={onCourseCreated}>
         <label htmlFor="name">Course Name*</label>
-        <input type="text" id="name" name="name" placeholder="E.G. Introduction to Psychology"></input>
+        <input type="text" id="name" name="name" placeholder='E.G., "Introduction to Psychology"'></input>
         
         <label htmlFor="descr">Course Description*</label>
-        <textarea id="descr" name="descr" rows="3" placeholder="E.G. An Introductory Course Exploring Key Psychological Theories and Concepts"></textarea>
+        <textarea id="descr" name="descr" rows="3" placeholder='E.G., "An Introductory Course Exploring Key Psychological Theories and Concepts"'></textarea>
         
         <div className="modal-button-row">
-          <input type="button" value="Cancel"></input>
+          <input type="button" value="Cancel" onClick={onClose}></input>
           <input type="submit" value="Confirm"></input>
         </div>
       </form>
